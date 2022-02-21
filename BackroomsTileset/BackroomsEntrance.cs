58b77a198dfd7b6ed2045c769b97f899cff84f15
John 'Nielk1' Klein <development@nielk1.com>
2022-02-20 20:39:52 -0600

    test mod for new tileset/tiles added

---
+++ BackroomsTileset/BackroomsEntrance.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackroomsEntrance : MonoBehaviour
+{
+    public Jigsaw.Connector DoorConnector;
+    public Light DoorLight;
+
+    private bool Done = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (DoorConnector.connected)
+        {
+            Done = true;
+        }
+        else
+        {
+            //DoorLight.enabled = false;
+            DoorLight.intensity = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Done) return;
+        if(DoorConnector.connected && !DoorConnector.fallbackConnected)
+        {
+            //DoorLight.enabled = true;
+            DoorLight.intensity = 1;
+            Done = true;
+        }
+    }
+}
---
+++ BackroomsTileset/BackroomsTileset.cs
@@ -0,0 +1,46 @@
+﻿using AnythingGalleryLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BackroomsTileset
+{
+    public class BackroomsTileset : ITileset
+    {
+        public string Name => "Backrooms";
+        public string ResourceName => "backrooms";
+        public TileData[] Tiles => new TileData[] {
+            new TileData() {
+                Name = "BackroomsEntrance",
+                Tilesets = new string[]{ "Backrooms", "ModernGallery" }
+            },
+            new TileData() {
+                Name = "BackroomsHallway_01",
+                Tilesets = new string[]{ "Backrooms" },
+                ScannerCounts = new Dictionary<string, int>(){ { "image", 2 } }
+            },
+            new TileData() {
+                Name = "BackroomsHallway_02",
+                Tilesets = new string[]{ "Backrooms" },
+                ScannerCounts = new Dictionary<string, int>(){ { "image", 1 }, { "info", 1 } }
+            },
+        };
+
+        public void AttachMissingComponents(GameObject obj)
+        {
+            if (obj.name == "BackroomsEntrance")
+            {
+                Light doorLight = (Light)(obj.transform.Find("Spot Light")?.gameObject?.GetComponent<Light>());
+                Jigsaw.Connector connector = (Jigsaw.Connector)(obj.transform.Find("Connectors")?.Find("Connector")?.gameObject?.GetComponent<Jigsaw.Connector>());
+
+                if (connector != null && doorLight != null)
+                {
+                    BackroomsEntrance behavior = (BackroomsEntrance)obj.AddComponent(typeof(BackroomsEntrance));
+                    behavior.DoorLight = doorLight;
+                    behavior.DoorConnector = connector;
+                }
+            }
+        }
+    }
+}
